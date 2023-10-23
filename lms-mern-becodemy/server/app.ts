5b17fce1f7ac908904fac876798b10e171b3c4a2
SatyendraSinghDev <satyendrasinghdev2@gmail.com>
2023-10-24 05:15:15 +0530

    Added basic setup like required packages in backend, express server creation, api testing routes

---
+++ lms-mern-becodemy/server/app.ts
@@ -0,0 +1,27 @@
+import express, { NextFunction, Request, Response } from "express";
+export const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+require("dotenv").config();
+
+// body parser
+app.use(express.json({ limit: "50mb" }));
+
+// cookie parser - sending or set something from the  frontend into the backend cookie
+app.use(cookieParser());
+
+// cors - cross origin resource sharing, expose our api to the other server, we can also pass our origin or whitelist other server or IP
+// app.use(cors());
+app.use(cors({ origin: process.env.ORIGIN }));
+
+// Testing our api
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({ success: true, message: "API is working!!" });
+});
+
+// Unknown Routes this will give errors
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
